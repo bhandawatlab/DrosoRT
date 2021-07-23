9b5a36422741dcc9d5e3b4fda7db46c5efd67dae
Liangyu Tao <liangyu.tao1001@gmail.com>
2021-07-23 12:53:02 -0400

    Update convertToUnitCircle.m

--- Util/convertToUnitCircle.m
+++ Util/convertToUnitCircle.m
@@ -1,27 +1,27 @@
 function [Data] = convertToUnitCircle(Data,Arena)
 
-for i = 1:size(Data.x,1)
-    xTmp = Data.x(i,:);
-    yTmp = Data.y(i,:);
+for i = 1:size(Data.Center.x,1)
+    xTmp = Data.Center.x(i,:);
+    yTmp = Data.Center.y(i,:);
     x = interp1q(find(xTmp~=0)',xTmp(xTmp~=0)',[1:1:length(xTmp)]')';
     y = interp1q(find(yTmp~=0)',yTmp(yTmp~=0)',[1:1:length(yTmp)]')';
-    Data.x(i,:) = x;Data.y(i,:) = y;
+    Data.Center.x(i,:) = x;Data.Center.y(i,:) = y;
 end
 
-x = Data.x;y = Data.y;
+x = Data.Center.x;y = Data.Center.y;
 
-xH = Data.xHead;
-yH = Data.yHead;
+xH = Data.Head.x;
+yH = Data.Head.y;
 
 for i = 1:size(x,1)
-    x(i,:) = (x(i,:)-Arena.center(1,i))./Arena.rad(i);
-    y(i,:) = (y(i,:)-Arena.center(2,i))./Arena.rad(i);
-    xH(i,:) = (xH(i,:)-Arena.center(1,i))./Arena.rad(i);
-    yH(i,:) = (yH(i,:)-Arena.center(2,i))./Arena.rad(i);
+    x(i,:) = (x(i,:)-Arena.arenaCent(1,i))./Arena.rad(i);
+    y(i,:) = (y(i,:)-Arena.arenaCent(2,i))./Arena.rad(i);
+    xH(i,:) = (xH(i,:)-Arena.arenaCent(1,i))./Arena.rad(i);
+    yH(i,:) = (yH(i,:)-Arena.arenaCent(2,i))./Arena.rad(i);
     
     rad = sqrt(x(i,:).^2+y(i,:).^2);
     
-    if sum(rad>0.9)>100
+    if sum(rad>0.85)>100
         x(i,:) = x(i,:)./max(rad);
         y(i,:) = y(i,:)./max(rad);
         xH(i,:) = xH(i,:)./max(rad);
@@ -30,9 +30,9 @@ for i = 1:size(x,1)
     
 end
 
-Data.x = x;
-Data.y = y;
-Data.xHead = xH;
-Data.yHead = yH;
+Data.Center.x = x;
+Data.Center.y = y;
+Data.Head.x = xH;
+Data.Head.y = yH;
 
 end
